35588f0b172b1775ef90eaeec585dd4608d978c8
Nordine Bittich <nordine@bittich.be>
2023-12-01 12:04:10 +0100

    wip

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import { app } from "mu";
 import bodyParser from "body-parser";
-import { URLSearchParams } from "url";
 import fetch from "node-fetch";
+
+import {
+  check,
+  checkNotNull,
+  selectAccount,
+  selectGroup,
+  selectRoles,
+  selectAccountBySession,
+  removeOldSession,
+  insertNewSessionForAccount,
+} from "./lib/utils";
 
 const HEADER_MU_SESSION_ID = "mu-session-id";
 const HEADER_X_REWRITE_URL = "x-rewrite-url";
 const ACM_IDM_LOGIN_ENDPOINT =
   process.env.ACM_IDM_LOGIN_ENDPOINT || "http://login";
+
+const CONTROLLER_ROLE = process.env.CONTROLLER_ROLE || "ControllerWOP";
+
 app.use(
   bodyParser.json({
     type: function(req) {
@@ -15,34 +28,137 @@ app.use(
   }),
 );
 
-app.get("/", async function(req, res, next) {
+app.delete("/sessions/current", async function(req, res, next) {
   try {
-    const code = req.query["code"];
-    console.log("sess", req.get(HEADER_MU_SESSION_ID));
-    console.log("code", code);
-    const formData = new URLSearchParams();
-    formData.append("authorizationCode", code);
-    const response = await fetch(ACM_IDM_LOGIN_ENDPOINT + "/sessions", {
-      method: "post",
-      body: formData,
-      headers: {
-        //"Content-Type": "application/json",
-        "mu-session-id": req.get(HEADER_MU_SESSION_ID),
-        "x-rewrite-url": req.get(HEADER_X_REWRITE_URL),
-        "mu-call-id": req.get("mu-call-id"),
-      },
-    });
+    const sessionUri = req.get(HEADER_MU_SESSION_ID);
+    checkNotNull(sessionUri, "session uri missing");
+    const { accountUri } = await selectAccountBySession(sessionUri);
+    checkNotNull(accountUri, "invalid session");
+    await removeOldSession(sessionUri);
 
-    console.log("response: ", await response.text());
-    console.log("status: ", response.status);
-    return res.status(403).send(response);
+    return res.header("mu-auth-allowed-groups", "CLEAR").status(204).end();
   } catch (e) {
     return next(e);
   }
 });
 
+// todo just copy paste the code to do it here instead of calling the acm idm service
+app.get("/sessions/current", async function(req, res, next) {
+  try {
+    const currentSession = await getCurrentSession(
+      req.get(HEADER_MU_SESSION_ID),
+    );
+    if (currentSession.status !== 200) {
+      console.log("not logged in.");
+      return res.header("mu-auth-allowed-groups", "CLEAR").redirect("/logout");
+    } else {
+      return res.status(200).send(currentSession.body);
+    }
+  } catch (e) {
+    return next(e);
+  }
+});
+
+app.post("/sessions", async function(req, res, next) {
+  try {
+    const sessionUri = req.get(HEADER_MU_SESSION_ID);
+    checkNotNull(sessionUri, "sessionUri missing");
+    const rewriteUrl = req.get(HEADER_X_REWRITE_URL);
+    checkNotNull(rewriteUrl, "rewrite url missing");
+
+    const currentSession = await getCurrentSession(
+      req.get(HEADER_MU_SESSION_ID),
+    );
+    if (currentSession.status !== 200) {
+      console.log("not logged in.");
+      return res.header("mu-auth-allowed-groups", "CLEAR").redirect("/logout");
+    }
+    const roles = currentSession?.body?.data?.attributes?.roles;
+
+    if (roles.includes(CONTROLLER_ROLE)) {
+      const data = req.body.data;
+      checkNotNull(data, "data is null");
+      check(data.type === "sessions", "data type is invalid");
+      check(
+        data.id === null || data.id === undefined,
+        "Id paramater is not allowed",
+      );
+      const accountId = data.relationships?.account?.data?.id;
+      const groupId = data.relationships?.group?.data?.id;
+
+      checkNotNull(accountId, "exactly one account should be linked");
+      checkNotNull(groupId, "exactly one group should be linked");
+
+      const account = await selectAccount(accountId);
+      checkNotNull(account, "missing account " + accountId);
+      const group = await selectGroup(groupId);
+      checkNotNull(group, "missing group " + groupId);
+      const roles = await selectRoles(accountId);
+      checkNotNull(roles, "missing roles " + accountId);
+
+      await removeOldSession(sessionUri);
+      const { sessionId } = await insertNewSessionForAccount(
+        account,
+        sessionUri,
+        group,
+        roles,
+      );
+
+      return res
+        .header("mu-auth-allowed-groups", "CLEAR")
+        .status(201)
+        .send({
+          links: {
+            self: "/sessions/current",
+          },
+          data: {
+            type: "sessions",
+            id: sessionId,
+            attributes: {
+              roles: roles,
+            },
+          },
+          relationships: {
+            account: {
+              links: { related: `/accounts/${accountId}` },
+              data: { type: "accounts", id: accountId },
+            },
+            group: {
+              links: { related: `/bestuurseenheden/${groupId}` },
+              data: { type: "bestuurseenheden", id: groupId },
+            },
+          },
+        });
+    } else {
+      console.log("role ", CONTROLLER_ROLE, "missing in ", roles);
+      return res.header("mu-auth-allowed-groups", "CLEAR").redirect("/logout");
+    }
+  } catch (e) {
+    return next(e);
+  }
+});
+
+async function getCurrentSession(sessionId) {
+  const currentSessionResponse = await fetch(
+    ACM_IDM_LOGIN_ENDPOINT + "/sessions/current",
+    {
+      method: "get",
+      headers: {
+        "mu-session-id": sessionId,
+      },
+    },
+  );
+  if (currentSessionResponse.status !== 200) {
+    return {
+      status: currentSessionResponse.status,
+      body: currentSessionResponse.statusText,
+    };
+  }
+  const json = await currentSessionResponse.json();
+  return { status: 200, body: json };
+}
 function error(err, _req, res, _next) {
-  console.error(err.stack);
+  console.log("error!: ", err);
   return res.status(500).send("Something broke!");
 }
 
